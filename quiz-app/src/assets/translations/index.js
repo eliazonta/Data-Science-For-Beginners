cb210cd08e2f54e95fe718681c9d4b3e5d3dba3c
happyeliia <78420183+eliazonta@users.noreply.github.com>
2022-04-21 21:38:04 +0200

    adding it quizzes
    added Italian quizzes (files in directory are WIP)

--- quiz-app/src/assets/translations/index.js
+++ quiz-app/src/assets/translations/index.js
@@ -1,10 +1,12 @@
 import englishQuizzes from './en/';
 import frenchQuizzes from './fr/';
 import spanishQuizzes from './es/';
+import italianQuizzes from './it/';
 
 const messages = {
   en: englishQuizzes,
   fr: frenchQuizzes,
   es: spanishQuizzes,
+  it: italianQuizzes,
 };
 export default messages;
